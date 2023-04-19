7f8429efdd747aa25cd9e8a30062278f0d5af280
Felix <fs146@hdm-stuttgart.de>
2023-04-19 19:05:36 +0200

    Backend: Add missing CRUD options; Add logging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,39 +1,91 @@
 import { serve } from "$std/http/server.ts";
 import { Hono } from "hono/mod.ts";
+import { jwt } from "hono/middleware.ts"
 import Database from "./core/db.ts";
 
+import { info } from "./core/log.ts";
 
 // Configure database
 const db = new Database();
 
-// Signin as a namespace, database, or root user
+// Signin to namespace, database, and user
+info("DB", "Signing in")
 await db.signin({
-	user: 'fme',
-	pass: 'ba7825d4',
+	user: "fme",
+	pass: "ba7825d4",
 });
 
 // Select a specific namespace / database
+info("DB", "Configuring...")
 await db.use('fme', 'fme');
 
 
 // Configure web server
-const app = new Hono();
+info("Hono", "Registering routes...")
+export const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Deno!"));
+// Require authentication for all routes except /users/auth
+// app.use("*", jwt({
+// 	secret: "some-secrety-secret",
+// }));
+
+app.all("/", (c) => c.json({ msg: "Hello Deno!" }));
 
 app
 	.get("/:collection", async (c) => {
 		const collection = c.req.param("collection");
 
-		if (collection)
+		if (collection) {
 			return c.json(await db.select(collection));
+		}
 	})
+	.post("/:collection", async (c) => {
+		const collection = c.req.param("collection");
+
+		if (collection) {
+			return c.json(await db.create(collection), await c.req.json());
+		}
+	});
+
+app
 	.get("/:collection/:id", async (c) => {
 		const collection = c.req.param("collection");
 		const id = c.req.param("id");
 
-		if (collection && id)
+		if (collection && id) {
 			await db.select(collection + ":" + id);
+		}
+	})
+	.post("/:collection/:id", async (c) => {
+		const collection = c.req.param("collection");
+		const id = c.req.param("id");
+
+		if (collection && id)
+			return c.json(await db.create(collection + ":" + id, await c.req.json()));
+	})
+	.put("/:collection/:id", async (c) => {
+		const collection = c.req.param("collection");
+		const id = c.req.param("id");
+
+		if (collection && id) {
+			return c.json(await db.update(collection + ":" + id, await c.req.json()));
+		}
+	})
+	.patch("/:collection/:id", async (c) => {
+		const collection = c.req.param("collection");
+		const id = c.req.param("id");
+
+		if (collection && id) {
+			return c.json(await db.update(collection + ":" + id, await c.req.json()));
+		}
+	})
+	.delete("/:collection/:id", async (c) => {
+		const collection = c.req.param("collection");
+		const id = c.req.param("id");
+
+		if (collection && id) {
+			return c.json(await db.delete(collection + ":" + id));
+		}
 	});
 
 
