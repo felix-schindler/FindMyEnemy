283397d790941148d5fe7903eec7e842b8d2e206
Felix <fs146@hdm-stuttgart.de>
2023-05-02 10:27:21 +0200

    Backend: Use singleton for database

--- backend/src/core/db.ts
+++ backend/src/core/db.ts
@@ -1,7 +1,17 @@
 import { Surreal } from "surrealdb";
+import Log from "./log.ts";
 
 export default class Database extends Surreal {
-	constructor() {
+	public static shared = new Database();
+
+	private constructor() {
+		Log.info("DB", "Connecting");
 		super("http://localhost/db/rpc");
+		this.signin({
+			NS: "fme",
+			DB: "fme",
+			user: "fme",
+			pass: "ba7825d4",
+		});
 	}
 }
