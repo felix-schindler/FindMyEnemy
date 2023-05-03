da209ef54ee5b7e880ad994efe8f67a139c222f9
mrnxnoir <81383449+mrnxnoir@users.noreply.github.com>
2023-05-03 18:11:28 +0200

    Update data.ts
    Updated data.ts to all 35 questions

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -7,7 +7,7 @@ export const questions = [
     {
         question: "At a party do you",
         answers: [
-            "Interact with many, including stranegrs",
+            "Interact with many, including strangers",
             "Interact with a few, known to you",
         ],
     },
@@ -32,11 +32,228 @@ export const questions = [
             "Emotions",
         ],
     },
-        {
+    {
         question: "Are more drawn toward the",
         answers: [
             "Convincing",
             "Touching",
+        ],
+    },
+    {
+        question: "Do you prefer to work",
+        answers: [
+            "To deadlines",
+            "Just 'whenever'",
+        ],
+    },
+    {
+        question: "Do you need to choose",
+        answers: [
+            "Rather carefully",
+            "Somewhat impulsively",
+        ],
+    },
+    {
+        question: "At parties do you",
+        answers: [
+            "Stay late, with increasing energy",
+            "Leave early with decreased energy",
+        ],
+    },
+    {
+        question: "Are you more attracted to",
+        answers: [
+            "Sensible people",
+            "Imaginative people",
+        ],
+    },
+    {
+        question: "Are you more interested in",
+        answers: [
+            "What is actual",
+            "What is possible",
+        ],
+    },
+    {
+        question: "In judging others are you more swayed by",
+        answers: [
+            "Laws than circumstances",
+            "Circumstances than laws",
+        ],
+    },
+    {
+        question: "In approaching others is your inclination to be somewhat",
+        answers: [
+            "Objective",
+            "Personal",
+        ],
+    },
+    {
+        question: "Are you more",
+        answers: [
+            "Punctual",
+            "Leisurly",
+        ],
+    },
+    {
+        question: "Does it bother you more having things",
+        answers: [
+            "Incomplete",
+            "Complete",
+        ],
+    },
+    {
+        question: "In your social groups do you",
+        answers: [
+            "Keep abreast of other's happenings",
+            "Get behind on the news",
+        ],
+    },
+    {
+        question: "In doing ordinary things are you more likely to",
+        answers: [
+            "Do it the usual way",
+            "Do it your own way",
+        ],
+    },
+    {
+        question: "Writers should",
+        answers: [
+            "Say what they mean and mean what they say",
+            "Express things more by use of analogy",
+        ],
+    },
+    {
+        question: "Which appeals to you more",
+        answers: [
+            "Consistency of thought",
+            "Harmonious human relationships",
+        ],
+    },
+    {
+        question: "Are you more comfortable in making",
+        answers: [
+            "Logical judgments",
+            "Value judgments",
+        ],
+    },
+    {
+        question: "Do you want things",
+        answers: [
+            "Settled and decided",
+            "Unsettled and undecided",
+        ],
+    },
+    {
+        question: "Would you say you are more",
+        answers: [
+            "Serious and determined",
+            "Easy-going",
+        ],
+    },
+    {
+        question: "In phoning do you:",
+        answers: [
+            "Rarely question that it will all be said",
+            "Rehearse what you’ll say",
+        ],
+    },
+    {
+        question: "",
+        answers: [
+            "",
+            "",
+        ],
+    },
+    {
+        question: "Facts",
+        answers: [
+            "Speak for themselves",
+            "Illustrate principles",
+        ],
+    },
+    {
+        question: "Are visionaries",
+        answers: [
+            "Somewhat annoying",
+            "Rather fascinating",
+        ],
+    },
+    {
+        question: "Are you more often",
+        answers: [
+            "A cool-headed person",
+            "A warm-hearted person",
+        ],
+    },
+    {
+        question: "Is it worse to be",
+        answers: [
+            "Unjust",
+            "Merciless",
+        ],
+    },
+    {
+        question: "Should one usually let events occur",
+        answers: [
+            "By careful selection and choice",
+            "Randomly and by chance",
+        ],
+    },
+    {
+        question: "Do you feel better about",
+        answers: [
+            "Having purchased",
+            "Having the option to buy",
+        ],
+    },
+    {
+        question: "In company do you",
+        answers: [
+            "Initiate conversation",
+            "Wait to approached",
+        ],
+    },
+    {
+        question: "Common sense is",
+        answers: [
+            "Rarely questionable",
+            "Frequently questionable",
+        ],
+    },
+    {
+        question: "Children often do not",
+        answers: [
+            "Make themselves useful enough",
+            "Excercise their fantasy enough",
+        ],
+    },
+    {
+        question: "In making decisions do you feel more comfortable with",
+        answers: [
+            "Standards",
+            "Feelings",
+        ],
+    },
+    {
+        question: "Are you more",
+        answers: [
+            "A firm than gentle",
+            "Gentle than firm",
+        ],
+    },
+    {
+        question: "Which is more admirable",
+        answers: [
+            "The ability to organize and be methodical",
+            "The ability to adapt and make do",
+        ],
+    },
+    {
+        question: "Do you put more value on",
+        answers: [
+            "Infinite",
+            "Open-minded",
         ],
     },
 ]
