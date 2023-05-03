65fd954121888360ba40fb0c2a02414ba0e258a5
mrnxnoir <81383449+mrnxnoir@users.noreply.github.com>
2023-05-03 14:41:18 +0200

    Create Data.ts with Testdate

---
+++ frontend/src/lib/data.ts
@@ -0,0 +1,37 @@
+export const questions = [
+    {
+        question: "At a party do you",
+        answers: [
+            "Interact with many, including stranegrs",
+            "Interact with a few, known to you",
+        ],
+    },
+    {
+        question: "Are you more",
+        answers: [
+            "Realistic than speculative",
+            "Speculative than realistic",
+        ],
+    },
+    {
+        question: "Is it worse to",
+        answers: [
+            "Have your 'head in the clouds'",
+            "Be 'in a rut'",
+        ],
+    },
+    {
+        question: "Are you more impressed by",
+        answers: [
+            "Principles",
+            "Emotions",
+        ],
+    },
+        {
+        question: "Are more drawn toward the",
+        answers: [
+            "Convincing",
+            "Touching",
+        ],
+    },
+]
