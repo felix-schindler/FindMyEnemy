68feee011ac9c03b34a6fe8930dc57bcd01a74f2
ayseero <58523766+ayseero@users.noreply.github.com>
2023-05-17 19:56:31 +0200

    created enemy account page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -11,13 +11,13 @@
 </div>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;0,700;1,700&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,700&display=swap');
 	.app {
 		background: var(--background-gradient);
 		color: var(--text-icon);
 		display: grid;
 		font-family: 'Rubik', sans-serif;
-		font-weight: 500;
+		font-weight: 700;
 		font-style: normal;
 		/*font-family: var(--font-family);
 		font-weight: var(--fw-bold);*/
