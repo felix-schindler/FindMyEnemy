f29ddd526e3d76d4524e6db5bfd5522c5e22b3e5
Felix <fs146@hdm-stuttgart.de>
2023-05-21 21:51:25 +0200

    Backend(types): adjust to new db

--- backend/src/core/types.ts
+++ backend/src/core/types.ts
@@ -1,7 +1,12 @@
+// The most simple API answer
+export type Status = {
+	success: boolean;
+	message: string;
+};
+
 export type User = {
 	id: number;
 	username: string;
-	email: string;
 	password: string;
 	personality: string;
 };
@@ -9,14 +14,12 @@ export type User = {
 export type ClientUser = {
 	id: number;
 	username: string;
-	email: string;
 	personality: string;
 };
 
 export type AuthUser = {
 	id: number;
 	username: string;
-	email: string;
 	personality: string;
 	token: string;
 };
@@ -24,17 +27,30 @@ export type AuthUser = {
 export type Question = {
 	id: number;
 	content: string;
-	answers: Answer[];
-	category_id: number;
 };
 
-type Answer = {
+export type Category = "E" | "I" | "S" | "N" | "T" | "F" | "J" | "P";
+
+export type Answer = {
+	id: number;
+	question_id: number;
+	content: string;
+	category: Category;
+};
+
+export type ClientQuestion = {
 	id: number;
 	content: string;
+	answers: ClientAnswer[];
+};
+
+type ClientAnswer = {
+	id: number;
+	content: string;
+	category: Category;
 };
 
 export type UserAnswer = {
 	question_id: number; // Number between 0-34 (or maybe 1-35?)
-	answer_id: number; // Number between 0-1 (or maybe 1-2?)
-	category_id: number; // Number between 0-6 (or maybe 1-7?)
+	category: Category;
 };
