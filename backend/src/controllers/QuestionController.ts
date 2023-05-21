f85d95dfc1fd233507f95e86c4c774d976faf8ee
Felix <fs146@hdm-stuttgart.de>
2023-05-21 21:50:35 +0200

    Backend(questions): Replace queries & logic

--- backend/src/controllers/QuestionController.ts
+++ backend/src/controllers/QuestionController.ts
@@ -4,15 +4,32 @@ import Controller from "./Controller.ts";
 import { db } from "../core/Database.ts";
 import HttpError from "../core/HttpError.ts";
 
-import type { Question, UserAnswer } from "../core/types.ts";
+import type { Category, ClientQuestion, UserAnswer } from "../core/types.ts";
 
 export default class QuestionController extends Controller {
 	public static readonly shared = new QuestionController();
-	protected override readonly table = "questions";
 
 	public async getList(c: Context<Env, "/questions">): Promise<Response> {
-		const questions = await db.select<Question, string>(this.table);
-		return c.json(questions);
+		// Get questions with their answers from database
+		const dbQuestions = (await db.queryObject<ClientQuestion>(`
+			SELECT
+				q.id,
+				q.content,
+				jsonb_agg(jsonb_build_object(
+					'id', a.id,
+					'content', a.content,
+					'category', a.category
+				)) AS answers
+			FROM
+				question q
+				JOIN answer a ON q.id = a.question_id
+			GROUP BY
+				q.id, q.content
+			ORDER BY
+				q.id ASC;
+		`)).rows;
+
+		return c.json<ClientQuestion[]>(dbQuestions);
 	}
 
 	public async personality(
@@ -20,37 +37,32 @@ export default class QuestionController extends Controller {
 	): Promise<Response> {
 		const userAnswers = await c.req.json() as UserAnswer[];
 
-		if (!userAnswers) throw new HttpError(400, "Missing user answers");
+		// Validate user answers
+		if (!userAnswers || userAnswers.length < 35) {
+			throw new HttpError(400, "Missing user answers");
+		}
 
 		// Sum up answers
-		const sums: Record<number, number[]> = {};
-		userAnswers.map((a) => {
-			// Init array if it doesn't exist
-			if (!sums[a.category_id]) sums[a.category_id] = [0, 0];
-
-			// Add answer to array
-			sums[a.category_id][a.answer_id]++;
-		});
-
-		// Calculate personality
-		const personaliyScores = {
-			E: sums[0][0],
-			I: sums[0][1],
-			S: sums[1][0] + sums[2][0],
-			N: sums[1][1] + sums[2][1],
-			T: sums[3][0] + sums[4][0],
-			F: sums[3][1] + sums[4][1],
-			J: sums[5][0] + sums[6][0],
-			P: sums[5][1] + sums[6][1],
+		const sums: Record<Category, number> = {
+			E: 0,
+			I: 0,
+			S: 0,
+			N: 0,
+			T: 0,
+			F: 0,
+			J: 0,
+			P: 0,
 		};
 
-		// Get which letter has the hightest score
-		const personality = Object.keys(personaliyScores).reduce((a, b) =>
-			// TODO: I don't know if this works, let me test it first
-			// @ts-ignore - I don't know if this works, let me test it first
-			personaliyScores[a] > personaliyScores[b] ? a : b
-		);
+		userAnswers.map((a) => {
+			sums[a.category]++;
+		});
 
+		// Get which letter has the hightest score
+		const personality =
+			Object.entries(sums).reduce((a, b) => a[1] > b[1] ? a : b)[0];
+
+		// Return the personality
 		return c.text(personality);
 	}
 }
