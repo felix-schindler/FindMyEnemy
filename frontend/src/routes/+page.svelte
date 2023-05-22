53ca0c6dee3229e5b91146bebc81a0fd5f1b7ebe
vianhnguyen <vn013@hdm-stuttgart.de>
2023-05-22 14:44:06 +0200

    top enemies page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,3 +6,9 @@
 
 <a href="/auth">Authentication stuff</a>
 <a href="/personality-test">Take the test</a>
+<<<<<<< Updated upstream
+=======
+<a href="/onboarding">Onboarding</a>
+<a href="/homepage">Homepage</a>
+<a href="/top-enemies">Top enemies</a>
+>>>>>>> Stashed changes
