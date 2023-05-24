0743bf63579d92705860313cb2052521a129115f
Felix <fs146@hdm-stuttgart.de>
2023-05-24 19:35:52 +0200

    B(challange): Add tests for GET (list / single), POST, PATCH, DELETE

--- backend/src/tests/challenges_test.ts
+++ backend/src/tests/challenges_test.ts
@@ -1,71 +1,111 @@
-/*
-import { app } from "../main.ts";
-import { assertEquals, assertNotEquals } from "$std/testing/asserts.ts";
-import { Challenge } from "../core/types.ts";
+import { ac, app } from "../main.ts";
+import {
+	assertEquals,
+	assertNotEquals,
+	assertObjectMatch,
+} from "$std/testing/asserts.ts";
+import type { AuthUser, Challange, Status } from "../core/types.ts";
 
-Deno.test("Challenges", async () => {
-	// deno-lint-ignore no-explicit-any
-	let res: Response, body: any;
+// #region Setup
+// deno-lint-ignore no-explicit-any
+let res: Response, body: any, tester: Challange;
 
-	// #region Login
+const authUser = await ((await app.request("/users/login", {
+	method: "POST",
+	body: JSON.stringify({
+		username: "admin",
+		password: "admin",
+	}),
+})).json()) as AuthUser;
 
-	res = await app.request("/users/login", {
-		method: "POST",
-		body: JSON.stringify({
-			user: "admin",
-			password: "admin",
-		}),
-	});
+const AUTH_TOKEN = authUser.token;
+// #endregion Setup
 
-	const AUTH_TOKEN = (await res.json()).token;
-
-	assertEquals(res.status, 200);
-	assertNotEquals(AUTH_TOKEN, undefined);
-
-	// #endregion Login
-
-	// #region Challenges
-
-	res = await app.request("/challenges", {
-		headers: {
-			Authorization: AUTH_TOKEN,
-		}
-	});
-
-	assertEquals(res.status, 200);
-
-	body = await res.json();
-	assertEquals(body.length, 0);
-
-	// #endregion Challenges
-
-	// #region Create challenge
-
-	const NEW_CHALLENGE: Challenge = {
-		self_user_id: 1,
-		user_id: 1,
-		self_score: 0,
-		opponent_score: 0,
+Deno.test("Create challange", async () => {
+	const NEW_CHALLANGE = {
+		score: 5,
+		challangee: 2,
 	};
 
-	res = await app.request("/users/1/challenge", {
-		method: "POST",
-		body: JSON.stringify(NEW_CHALLENGE)
-	})
-
-	assertEquals(res.status, 401);
-
-	res = await app.request("/users/1/challenge", {
+	res = await app.request("/users/1/challange", {
 		method: "POST",
 		headers: {
 			Authorization: AUTH_TOKEN,
 		},
-		body: JSON.stringify(NEW_CHALLENGE)
+		body: JSON.stringify(NEW_CHALLANGE),
 	});
+	body = await res.json() as Challange;
 
 	assertEquals(res.status, 200);
-	assertNotEquals((await res.json()).id, undefined);
+	assertNotEquals(body.id, undefined);
+	assertNotEquals(body.id, 0);
+	assertEquals(body.user_1_id, authUser.id);
+	assertEquals(body.user_2_id, NEW_CHALLANGE.challangee);
+	assertEquals(body.user_1_score, NEW_CHALLANGE.score);
+	assertEquals(body.user_2_score, 0);
 
-	// #endregion Create challenge
+	tester = body;
 });
-*/
+
+Deno.test("List challanges", async () => {
+	res = await app.request("/challanges", {
+		headers: {
+			"Authorization": AUTH_TOKEN,
+		},
+	});
+	body = await res.json();
+
+	assertEquals(res.status, 200);
+	assertNotEquals(body.length, 0);
+});
+
+Deno.test("Get challange", async () => {
+	res = await app.request(`/challanges/${tester.id}`, {
+		headers: {
+			"Authorization": AUTH_TOKEN,
+		},
+	});
+	body = await res.json() as Challange;
+
+	assertEquals(res.status, 200);
+	assertObjectMatch(body, tester);
+});
+
+Deno.test("Update challange", async () => {
+	const score = 10;
+
+	res = await app.request(`/challanges/${tester.id}`, {
+		method: "PATCH",
+		headers: {
+			Authorization: AUTH_TOKEN,
+		},
+		body: JSON.stringify({ score }),
+	});
+	body = await res.json() as Challange;
+
+	assertEquals(res.status, 200);
+	assertEquals(body.user_1_id, tester.user_1_id);
+	assertEquals(body.user_2_id, tester.user_2_id);
+	if (body.user_1_id == authUser.id) {
+		assertEquals(body.user_1_score, score);
+		assertEquals(body.user_2_score, tester.user_2_score);
+	} else {
+		assertEquals(body.user_2_score, score);
+		assertEquals(body.user_1_score, tester.user_1_score);
+	}
+});
+
+Deno.test("Delete challange", async () => {
+	res = await app.request(`/challanges/${tester.id}`, {
+		method: "DELETE",
+		headers: {
+			Authorization: AUTH_TOKEN,
+		},
+	});
+	body = await res.json() as Status;
+
+	assertEquals(res.status, 200);
+	assertEquals(body.raw.rowCount, 1);
+});
+
+ac.abort();
