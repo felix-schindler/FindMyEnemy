0ea3d717103d88fba8f3e994b25628787305a0ca
Felix <fs146@hdm-stuttgart.de>
2023-05-24 16:47:36 +0200

    Frontend: Update API client and types (incl. req map)

--- frontend/src/lib/core/types.ts
+++ frontend/src/lib/core/types.ts
@@ -1,24 +1,39 @@
-// Base records
-export type Record = {
-	id: string;
+export interface Status {
+	status: number;
+	message: string;
+	raw: unknown;
+}
+
+// "Database" types
+type Base = {
+	id: number;
 };
 
-// User records (auth)
-export type AuthRecord = Record & {
-	email: string;
+// Needs password when using an auth route
+export type User = Base & {
 	username: string;
 	personality: string;
-	jwt: string;
 };
 
-// Custom records
-export type Question = Record & {
-	question: string;
-	answers: string[];
+export type AuthUser = User & {
+	token: string;
 };
 
-export type User = Record & {
-	email: string;
-	username: string;
-	personality: string;
+type Category = 'E' | 'I' | 'S' | 'N' | 'T' | 'F' | 'J' | 'P';
+
+export type Question = {
+	id: number;
+	content: string;
+	answers: Answer[];
+};
+
+type Answer = {
+	id: number;
+	content: string;
+	category: Category;
+};
+
+export type UserAnswer = {
+	question_id: number;
+	category: Category;
 };
