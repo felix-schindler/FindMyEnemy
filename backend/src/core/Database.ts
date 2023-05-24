25ebe887ae722a93db1b1e73294ac0651b0f750e
Felix <fs146@hdm-stuttgart.de>
2023-05-24 19:34:09 +0200

    B(db): Remove log "connecting"

--- backend/src/core/Database.ts
+++ backend/src/core/Database.ts
@@ -1,12 +1,9 @@
 import { Client } from "postgres";
-import Log from "./Log.ts";
 
 export default class Database extends Client {
 	public static shared = new Database();
 
 	private constructor() {
-		Log.info("DB", "Connecting");
-
 		super({
 			user: "postgres",
 			password: "DnxctEotDlNsk3FuOoVm",
