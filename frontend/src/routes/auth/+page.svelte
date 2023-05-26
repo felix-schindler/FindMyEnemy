0f363691fe0a2538c4ce79da833e645650e8b629
mrnxnoir <mb421@hdm-stuttgart.de>
2023-05-26 18:12:42 +0200

    changed link path

--- frontend/src/routes/auth/+page.svelte
+++ frontend/src/routes/auth/+page.svelte
@@ -41,9 +41,7 @@
 		<input type="checkbox" id="remember" />
 		<label for="remember">Remember me</label>
 	</div>
-	<button type="submit" class="mainBtn" on:click={() => (window.location.href = '/homepage')}>
-		<span>Sign In</span>
-	</button>
+	<a href="/homepage" class="mainBtn">Sign In</a>
 	<a href="/onboarding"> Don't have an account yet? <b>Take the test! </b> </a>
 </form>
 
