d38718b358a098267e7c3ea4f006601cd9a5d2df
Cornelius Kiefer <ck184@hdm-stuttgart.de>
2023-06-15 12:53:06 +0200

    update ci yml

--- backend/src/core/Database.ts
+++ backend/src/core/Database.ts
@@ -8,7 +8,7 @@ export default class Database extends Client {
 			user: "postgres",
 			password: "DnxctEotDlNsk3FuOoVm",
 			database: "postgres",
-			hostname: Deno.env.get("DEV") ? "localhost" : "db",
+			//hostname: Deno.env.get("DEV") ? "localhost" : "db",
 			port: 5432,
 		});
 	}
