dd0a56e1bd22514a463e49d3f95d33550bdb11aa
vianhnguyen <vn013@hdm-stuttgart.de>
2023-06-17 02:18:09 +0200

    style back to page

--- frontend/src/routes/homepage/+page.svelte
+++ frontend/src/routes/homepage/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '$lib/style/main.css';
-	import '$lib/style/homepage.css';
 	import AccountButton from '$lib/components/AccountButton.svelte';
 	import { req } from '$lib/core/api';
 
@@ -79,4 +78,97 @@
 
 	
 </main>
+
+<style>
+	.searchBar {
+	display: flex;
+	justify-content: center;
+	margin-bottom: 32px;
+}
+
+.search-bar {
+	width: 80vw;
+	height: 20px;
+}
+
+.top-enemies {
+	margin: var(--margin40);
+}
+
+ .grid-container,
+.grid-container2 {
+	display: flex;
+	gap: var(--margin20);
+	 overflow-x: auto; 
+} 
+
+.grid-item {
+	flex: 0 0 clamp(30px, 50vw, 300px);
+	position: relative;
+	overflow: hidden;
+	/* margin-right: var(--gap-width);
+	margin-bottom: 20px; */
+	background-color: var(--primary);
+	padding: var(--padding);
+	border-radius: 10%;
+}
+
+/* .grid-item img {
+	width: 100%;
+	height: auto;
+	display: block;
+	transition: transform 0.3s ease-in-out;
+} */
+
+/* .user-information {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	width: 100%;
+	padding: var(--padding);
+	color: #fff;
+}
+
+.user-details {
+	display: flex;
+	justify-content: space-between;
+}
+
+.user-information h3,
+.user-information p {
+	margin: 0;
+}
+
+.grid-item:hover img {
+	transform: scale(1.1);
+}
+
+.buttons {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: auto;
+	width: 100%;
+} */
+
+
+.moredetails-button {
+	display: flex;
+	align-items: center;
+	/* margin-bottom: 16px; */
+}
+
+.moredetails-icon img {
+	/* display:inline-block; */
+	width: 8px;
+	/* Adjust the width and height as needed */
+	height: 12px;
+	/* background-image: url(/src/lib/images/moredetails.svg);    */
+	/* background-size: cover;  */
+	margin-left: var(--margin20);
+}
+
+
+
+</style>
 
