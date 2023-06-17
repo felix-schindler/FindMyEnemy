cf39b361def6af1bfb7bfcc978705d658523b7bb
vianhnguyen <vn013@hdm-stuttgart.de>
2023-06-17 23:01:48 +0200

    modified DiscoverEnemy ( load photo based on personality ), removed buttons from homepage

--- frontend/src/lib/components/DiscoverEnemy.svelte
+++ frontend/src/lib/components/DiscoverEnemy.svelte
@@ -1,23 +1,63 @@
 <script lang="ts">
-    export let user: {
-        id: number;
-        imageSrc: string;
-        title: string;
-        compatibility: string;
-        description: string;
-    };
-  </script>
-  
-  <div class="grid-item">
-    <img src={user.imageSrc} alt={user.title} />
-    <div class="user-information">
-      <div class="user-details">
-        <p><b>{user.title}</b></p>
-        <p><b>{user.compatibility}</b></p>
-      </div>
-      <p>{user.description}</p>
+  export let user: {
+      id: number;
+      personality: string;
+      title: string;
+      // compatibility: string;
+      // description: string;
+  };
+
+  function getImageSource(personality: string) {
+  if (personality === 'ENFJ') {
+    return 'ENFJ.svg';
+  } else if (personality === 'ENFP') {
+    return 'ENFP.svg';
+  } else if (personality === 'ENFP') {
+    return 'ENFP.svg';
+  } else if (personality === 'ENTJ'){
+  return 'ENTJ.svg'
+} else if (personality === 'ENTP'){
+  return 'ENTP.svg'
+  } else if (personality === 'ESFJ'){
+  return 'ESFJ.svg'
+} else if (personality === 'ESFP'){
+  return 'ESFP.svg'
+} else if (personality === 'ESTJ'){
+  return 'ESTJ.svg'
+} else if (personality === 'ESTP'){
+  return 'ESTP.svg'
+} else if (personality === 'INFJ'){
+  return 'INFJ.svg'
+} else if (personality === 'INFP'){
+  return  'INFP.svg'
+} else if (personality === 'INTJ'){
+  return 'INTJ.svg'
+} else if (personality === 'INTP'){
+  return 'INTP.svg'
+} else if (personality === 'ISFJ'){
+  return 'ISFJ.svg'
+} else if (personality === 'ISFP'){
+  return 'ISFP.svg'
+} else if (personality === 'ISTJ'){
+  return 'ISTJ.svg'
+} else if (personality === 'ISTP'){
+  return 'ISTP.svg'
+}
+ 
+}
+</script>
+
+<div class="grid-item">
+  <img src={getImageSource(user.personality)} alt={user.title} />
+  <div class="user-information">
+    <div class="user-details">
+      <p><b>{user.title}</b></p>
+      <!-- <p><b>{user.compatibility}</b></p> -->
     </div>
+    <p>{user.personality}</p>
   </div>
+</div>
+
 
 <style>
     .grid-item {
--- frontend/src/routes/homepage/+page.svelte
+++ frontend/src/routes/homepage/+page.svelte
@@ -6,120 +6,52 @@
 
 
 	import { onMount, onDestroy } from 'svelte';
-
-	let viewportWidth = 0;
-	let vwValue = 0;
-
-	let scrollPosition = 0;
-	let scrollPosition2 = 0;
-
-	onMount(() => {
-		const container = document.querySelector('.grid-container');
-		const container2 = document.querySelector('.grid-container2');
-		const buttonPrev = document.querySelector('.prevButton');
-		const buttonNext = document.querySelector('.nextButton');
-		const buttonPrev2 = document.querySelector('.prevButton2');
-		const buttonNext2 = document.querySelector('.nextButton2');
-
-		if (container && container2 && buttonPrev && buttonNext && buttonPrev2 && buttonNext2) {
-			const scrollLeft = () => {
-				scrollPosition -= container.clientWidth;
-				container.scrollTo({
-					left: scrollPosition,
-					behavior: 'smooth'
-				});
-			};
-
-			const scrollLeft2 = () => {
-				scrollPosition2 -= container2.clientWidth;
-				container2.scrollTo({
-					left: scrollPosition2,
-					behavior: 'smooth'
-				});
-			};
-
-			const scrollRight = () => {
-				scrollPosition += container.clientWidth;
-				container.scrollTo({
-					left: scrollPosition,
-					behavior: 'smooth'
-				});
-			};
-
-			const scrollRight2 = () => {
-				scrollPosition2 += container2.clientWidth;
-				container2.scrollTo({
-					left: scrollPosition2,
-					behavior: 'smooth'
-				});
-			};
-
-			buttonPrev.addEventListener('click', scrollLeft);
-			buttonNext.addEventListener('click', scrollRight);
-			buttonPrev2.addEventListener('click', scrollLeft2);
-			buttonNext2.addEventListener('click', scrollRight2);
-		}
-	});
-
-	$: {
-		viewportWidth = document.documentElement.clientWidth;
-	}
-
-	function getVWValue() {
-		const viewportWidth = document.documentElement.clientWidth;
-		const vwValue = (26 / viewportWidth) * 100;
-		return vwValue;
-	}
-
-	vwValue = getVWValue();
-
-	window.addEventListener('resize', () => {
-		vwValue = getVWValue();
-	});
-
-	const updateViewportWidth = () => {
-		viewportWidth = document.documentElement.clientWidth;
-	};
-
-	onMount(() => {
-		updateViewportWidth();
-		window.addEventListener('resize', updateViewportWidth);
-	});
-
-	onDestroy(() => {
-		window.removeEventListener('resize', updateViewportWidth);
-	});
-
+			
 	let users = [
 		{
 			id: 1,
-			imageSrc: '/src/lib/images/SpongeBob_SquarePants_character.svg.png',
 			title: 'Timothy',
+			personality: 'ENFJ',
 			compatibility: '89%',
 			description: '12km'
 		},
 		{
 			id: 2,
-			imageSrc: '/src/lib/images/SpongeBob_SquarePants_character.svg.png',
 			title: 'Benjamin',
+			personality: 'ENFP',
 			compatibility: '89%',
 			description: '12km'
 		},
 		{
 			id: 3,
-			imageSrc: '/src/lib/images/SpongeBob_SquarePants_character.svg.png',
-			title: 'Benjamin',
+			title: 'Natasha',
+			personality: 'INFJ',
 			compatibility: '89%',
 			description: '12km'
 		},
 		{
 			id: 4,
-			imageSrc: '/src/lib/images/SpongeBob_SquarePants_character.svg.png',
-			title: 'Benjamin',
+			title: 'Anna',
+			personality: 'ISTP',
+			compatibility: '89%',
+			description: '12km'
+		},
+		{
+			id: 5,
+			title: 'Kylie',
+			personality: 'ESFP',
+			compatibility: '89%',
+			description: '12km'
+		},
+		{
+			id: 6,
+			title: 'Dan',
+			personality: 'ISTJ',
 			compatibility: '89%',
 			description: '12km'
 		}
 	];
+
 </script>
 
 
@@ -127,8 +59,6 @@
 <main>
 	<AccountButton />
 
-	<!-- <p>The viewport width is: {viewportWidth}px</p>
-  <p>The vw value is: {vwValue}</p>  -->
 	<div class="searchBar">
 		<input type="search" class="search-bar" placeholder="Search..." />
 	</div>
@@ -138,11 +68,6 @@
 
 		<div class="moredetails-button">
 			<h2>Discover top enemies</h2>
-			<!-- <button class="moredetails-icon" on:click={() => {
-  window.location.href = '/top-enemies';
-}}>
-  <img src="/src/lib/images/moredetails.svg" alt="Button" /> 
-</button> -->
 
 			<button
 				class="moredetails-icon"
@@ -156,27 +81,12 @@
 
 		<div class="grid-container">
 			{#each users as user (user.id)}
-				<DiscoverEnemy {user} />
+			<DiscoverEnemy {user} />
 			{/each}
 		</div>
 	</div>
 
-	{#if viewportWidth > 601}
-		<div class="buttons">
-			<button class="reverseButton prevButton"
-				><img src="/src/lib/images/back-icon.svg" alt="Back" /></button
-			>
-			<button class="reverseButton nextButton"
-				><img src="/src/lib/images/next-icon.svg" alt="Next" /></button
-			>
-		</div>
-	{:else}
-		<div class="buttons-mobile">
-			<button class="reverseButton"><img src="/src/lib/images/back-icon.svg" alt="Back" /></button>
-			<button class="reverseButton"><img src="/src/lib/images/next-icon.svg" alt="Next" /></button>
-		</div>
-	{/if}
-
+	
 	<div class="top-enemies">
 		<div class="moredetails-button">
 			<h2>Your mortal enemies</h2>
@@ -185,25 +95,11 @@
 
 		<div class="grid-container2">
 			{#each users as user (user.id)}
-				<DiscoverEnemy {user} />
+			<DiscoverEnemy {user} />
 			{/each}
 		</div>
 	</div>
 
-	{#if viewportWidth > 601}
-		<div class="buttons">
-			<button class="reverseButton prevButton2"
-				><img src="/src/lib/images/back-icon.svg" alt="Back" /></button
-			>
-			<button class="reverseButton nextButton2"
-				><img src="/src/lib/images/next-icon.svg" alt="Next" /></button
-			>
-		</div>
-	{:else}
-		<div class="buttons-mobile">
-			<button class="reverseButton"><img src="/src/lib/images/back-icon.svg" alt="Back" /></button>
-			<button class="reverseButton"><img src="/src/lib/images/next-icon.svg" alt="Next" /></button>
-		</div>
-	{/if}
+	
 </main>
 
