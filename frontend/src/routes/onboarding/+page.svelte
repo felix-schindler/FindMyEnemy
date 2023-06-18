b12792e70faeb9de42e4a50c5fc6c6eb522ac12a
Felix <fs146@hdm-stuttgart.de>
2023-06-18 14:06:01 +0200

    Onboarding: Use test and result components

--- frontend/src/routes/onboarding/+page.svelte
+++ frontend/src/routes/onboarding/+page.svelte
@@ -1,39 +1,57 @@
-<script>
-	let screen = 1;
+<script lang="ts">
+	import PersonalityTest from './PersonalityTest.svelte';
+	import QuestionaireResult from './QuestionaireResult.svelte';
+
+	let screen = 3,
+		personality: string;
 
 	function handleNext() {
 		screen++;
 	}
 </script>
 
-{#if screen === 1}
-	<div>
-		<p>Find your enemies before they find you</p>
-		<h1>find my <br /> enemy</h1>
-		<button class="mainBtn" on:click={handleNext}
-			><span><img src="/src/lib/images/next-icon.svg" alt="Next" /></span></button
-		>
+{#if personality}
+	<QuestionaireResult {personality} />
+{:else if screen === 1}
+	<div class="layout">
+		<div>
+			<p>Find your enemies before they find you</p>
+			<h1>find my <br /> enemy</h1>
+			<button class="mainBtn" on:click={handleNext}>
+				<img src="/src/lib/images/next-icon.svg" alt="Next" />
+			</button>
+		</div>
 	</div>
-{/if}
-
-{#if screen === 2}
-	<div>
-		<h1>First, let us test your personality</h1>
-		<p>Based on the result, we will help you find your worst enemies.</p>
-		<button class="mainBtn" on:click={() => (window.location.href = '/personality-test')}
-			><span>Start test</span></button
-		>
+{:else if screen === 2}
+	<div class="layout">
+		<div>
+			<h1>First, let us test your personality</h1>
+			<p>Based on the result, we will help you find your worst enemies.</p>
+			<button class="mainBtn" on:click={handleNext}>
+				<span>Start test</span>
+			</button>
+		</div>
 	</div>
+{:else if screen === 3}
+	<PersonalityTest bind:personality />
 {/if}
 
 <style>
-	div {
-		width: 20%;
-		margin: 0 auto;
-		margin-top: 100px;
+	div.layout {
+		height: 100%;
 		display: flex;
 		flex-direction: column;
-		gap: 15px;
+		justify-content: end;
+	}
+
+	@media (min-width: 768px) {
+		div {
+			width: 20%;
+			margin-top: 100px;
+			display: flex;
+			flex-direction: column;
+			gap: 15px;
+		}
 	}
 
 	div button {
