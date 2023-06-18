2b68da1f4f56d4ff7af23bd232a8f73831ff42b3
Felix <fs146@hdm-stuttgart.de>
2023-06-18 20:14:21 +0200

    Re-implement auth redirects
    Closes #82

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,33 @@
-<script>
+<script lang="ts">
 	import '$lib/style/main.css';
-	import Header from './Header.svelte';
+
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+	import { redirect } from '@sveltejs/kit';
+	import { goto } from '$app/navigation';
+
+	import { authStore } from '$lib/core/stores';
+
 	import { Toaster } from 'svelte-french-toast';
+	import Header from './Header.svelte';
+
+	let path: string;
+	$: path = $page.url.pathname;
+
+	$: if (!$authStore && !path.startsWith('/auth')) {
+		const redir = '/auth?next=' + encodeURIComponent(path);
+		if (browser) goto(redir);
+		else redirect(307, redir);
+	}
 </script>
 
 <Toaster />
 <div class="app">
 	<Header />
 	<main>
-		<slot />
+		{#if $authStore || path.startsWith('/auth')}
+			<slot />
+		{/if}
 	</main>
 </div>
 
