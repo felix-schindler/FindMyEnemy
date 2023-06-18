53043520bb42eb36c2d2aa9509b5cca7b47e6dc2
Felix <fs146@hdm-stuttgart.de>
2023-06-18 13:07:17 +0200

    /+layout: Rm global font weight

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -13,17 +13,17 @@
 </div>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,700&display=swap');
 	.app {
-		background: var(--background-gradient);
-		color: var(--text-icon);
 		display: grid;
-		font-family: 'Rubik', sans-serif;
-		font-weight: 700;
-		font-style: normal;
-		/*font-family: var(--font-family);
-		font-weight: var(--fw-bold);*/
 		grid-template-rows: auto 1fr;
+
+		color: var(--text-icon);
+		background: var(--background-gradient);
+
+		font-family: 'Rubik', sans-serif;
+		font-style: normal;
+
 		min-height: 100vh;
+		padding: 2em;
 	}
 </style>
