74ed82a2b26c849eef8f4e6918d2098297a5f5ba
vianhnguyen <vn013@hdm-stuttgart.de>
2023-07-09 16:36:17 +0200

    comments

--- frontend/src/lib/components/DiscoverEnemy.svelte
+++ frontend/src/lib/components/DiscoverEnemy.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-	import type { User } from '$lib/core/types';
+    import type { User } from '$lib/core/types';
 
-	export let user: User;
+    export let user: User; // Declare a prop named 'user' of type User
 </script>
 
+<!-- Display an image based on the user's personality 
+	Display the user's username, personality and compatibility
+-->
+
 <a href="/enemy-account/{user.id}" class="grid-item">
-	<img src={`${user.personality}.svg`} alt={user.username} />
-	<div class="user-information">
-		<div class="user-details">
-			<p><b>{user.username}</b></p>
-			<p><b>{user.compatibility}%</b></p>
-		</div>
-		<p>{user.personality}</p>
-	</div>
+    <img src={`${user.personality}.svg`} alt={user.username} /> 
+    <div class="user-information">
+        <div class="user-details">
+            <p><b>{user.username}</b></p>
+            <p><b>{user.compatibility}</b></p> 
+        </div>
+        <p>{user.personality}</p> 
+    </div>
 </a>
+
 
 <style>
 	.grid-item {
